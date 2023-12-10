a7ac634488bfc76de5650c0d39b2cd662d1382d2
Any Buss <anykbuss@gmail.com>
2023-12-10 12:03:22 -0300

    feat: create home template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <script lang="ts" setup></script>
 
 <template>
-  <h1>HomePage</h1>
+  <v-main class="d-flex align-center justify-center">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" class="text-center">
+          <h1 class="text-h4">Conecte-se, Colabore e Cresça conosco!</h1>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-btn color="primary"> Novo Cooperado </v-btn>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn color="secondary"> Visualizar Cooperados </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
