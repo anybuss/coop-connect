d626f4726f5143af0dfd27bba65b219794925a5a
Any Buss <anykbuss@gmail.com>
2023-12-10 20:27:02 -0300

    feat: add edit user

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -7,8 +7,27 @@ import {
   prettifyDate,
   prettifyCurrency,
 } from "@/utils/prettifys";
+import UserForm from "@/components/users/UserForm.vue";
 
-const { users } = useUsersStore();
+type UserModel = {
+  id: string;
+  userType: "person" | "entity";
+  fullName: string;
+  phone: string;
+  taxId: string;
+  birthdateOrFoundationDate: string;
+  incomeOrRevenue: string;
+};
+
+const { users, editUser } = useUsersStore();
+
+const showEditModal = ref(false);
+const selectedUser = ref<UserModel | null>(null);
+
+const openEditUserForm = (user: UserModel) => {
+  selectedUser.value = user;
+  showEditModal.value = true;
+};
 
 const headers = [
   { title: "Nome", value: "fullName", sortable: true },
@@ -24,9 +43,28 @@ const headers = [
     title: "Renda/Faturamento",
     value: "incomeOrRevenue",
     sortable: true,
+    align: "end",
   },
   { title: "Ações", value: "id", sortable: false },
 ];
+
+const message = ref<string>("");
+const colorType = ref<string>("");
+const showSnackbar = ref<boolean>(false);
+const handleSubmitEdit = (formModel: any) => {
+  try {
+    editUser(formModel.id, formModel);
+    message.value = "Usuário editado com sucesso!";
+    colorType.value = "success";
+    showSnackbar.value = true;
+    showEditModal.value = false;
+  } catch (error: any) {
+    message.value = "Erro ao editar usuário!";
+    colorType.value = "error";
+    showSnackbar.value = true;
+    console.error(error.message);
+  }
+};
 </script>
 
 <template>
@@ -78,8 +116,40 @@ const headers = [
           </div>
         </template>
 
-        <template #item.id="{ item }"> </template>
+        <template #item.id="{ item }">
+          <v-row class="d-flex flex-nowrap">
+            <v-btn
+              size="x-small"
+              icon="mdi-pencil"
+              color="primary"
+              variant="tonal"
+              class="mr-2"
+              @click="openEditUserForm(item)"
+            ></v-btn>
+          </v-row>
+        </template>
       </v-data-table>
     </v-container>
+
+    <v-dialog v-model="showEditModal" width="800">
+      <v-card>
+        <v-container>
+          <h1 class="text-h4">Editar Cooperado</h1>
+          <p class="text-subtitle-1">O campo CPF/CNPJ não pode ser alterado.</p>
+        </v-container>
+        <v-container>
+          <UserForm :user="selectedUser" @submit-edit="handleSubmitEdit" />
+
+          <v-snackbar
+            close-on-content-click
+            :timeout="1500"
+            :color="colorType"
+            v-model="showSnackbar"
+          >
+            {{ message }}
+          </v-snackbar>
+        </v-container>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
