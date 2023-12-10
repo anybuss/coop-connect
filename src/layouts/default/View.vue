8cfced7ea9a027439908a790ae858bee78e1b6d7
Any Buss <anykbuss@gmail.com>
2023-12-10 10:57:22 -0300

    initial project config

---
+++ src/layouts/default/View.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const drawer = ref<boolean>(false);
+const toggleDrawer = () => {
+  drawer.value = !drawer.value;
+};
+</script>
+
+<template>
+  <v-navigation-drawer v-model="drawer" temporary> </v-navigation-drawer>
+
+  <v-app-bar flat class="border-b">
+    <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+    <v-app-bar-title>Coop Connect</v-app-bar-title>
+    <v-img max-width="60" src="@/assets/logo.png" />
+  </v-app-bar>
+
+  <v-main>
+    <router-view />
+  </v-main>
+</template>
