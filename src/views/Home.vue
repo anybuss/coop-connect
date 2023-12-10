9b175bd11012dfa834032fabcc9b046ef1262a9d
Any Buss <anykbuss@gmail.com>
2023-12-10 12:06:38 -0300

    feat: add redirects to routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,16 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goToNewUser = () => {
+  router.push({ name: "users-new" });
+};
+
+const goToShowUsers = () => {
+  router.push({ name: "users-show" });
+};
+</script>
 
 <template>
   <v-main class="d-flex align-center justify-center">
@@ -10,10 +22,12 @@
       </v-row>
       <v-row justify="center">
         <v-col cols="auto">
-          <v-btn color="primary"> Novo Cooperado </v-btn>
+          <v-btn color="primary" @click="goToNewUser"> Novo Cooperado </v-btn>
         </v-col>
         <v-col cols="auto">
-          <v-btn color="secondary"> Visualizar Cooperados </v-btn>
+          <v-btn color="primary" variant="outlined" @click="goToShowUsers">
+            Visualizar Cooperados
+          </v-btn>
         </v-col>
       </v-row>
     </v-container>
