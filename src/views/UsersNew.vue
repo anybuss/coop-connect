b9691ee7627df8992224488fe75a3d31848b0be3
Any Buss <anykbuss@gmail.com>
2023-12-10 15:56:32 -0300

    feat: add title and import user form on user page new

--- src/views/UsersNew.vue
+++ src/views/UsersNew.vue
@@ -1,5 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import UserForm from "@/components/users/UserForm.vue";
+
+// const handleSubmitForm(formModel: any){}
+</script>
 
 <template>
-  <h1>UsersNew</h1>
+  <v-container>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <h1 class="text-h4">Cadastro de Cooperado</h1>
+          <p class="text-subtitle-1">
+            Para cadastrar preencha o formulário abaixo.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container>
+      <UserForm @submit-form="" />
+    </v-container>
+  </v-container>
 </template>
