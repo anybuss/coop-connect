0bb3fd235cffd230ee7bb29324c5e4cb95a68802
Any Buss <anykbuss@gmail.com>
2023-12-15 17:30:03 -0300

    refac: add notification component

---
+++ src/components/AppNotification.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+interface Props {
+  show: boolean;
+  message: string;
+  color: string;
+  timeout?: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  show: false,
+  message: "",
+  color: "",
+  timeout: 3000,
+});
+</script>
+
+<template>
+  <v-snackbar
+    :timeout="props.timeout"
+    :color="props.color"
+    v-model="props.show"
+  >
+    {{ props.message }}
+  </v-snackbar>
+</template>
